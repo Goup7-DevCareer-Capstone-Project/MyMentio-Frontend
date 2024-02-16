0cd155640b5ee097fd46e804f6043eb3bdf2e189
Sandra Ojimmadu <uchennaojimmadu5@gmail.com>
2024-02-16 20:51:05 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/NavBar/navbar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Search from "./pages/Search";
