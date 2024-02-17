149ff8cb1a8a5edc2188dd5e7f4c5516a4221d38
Sandra Ojimmadu <uchennaojimmadu5@gmail.com>
2024-02-17 09:55:31 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Register from "./components/Account/Register";
 import Login from "./components/Account/Login";
 import Login2 from "./components/Account/Login2";
 import Register2 from "./components/Account/Register2";
+import  '../public/style.css';
 
 
 // import Header from "./components/Header/header";
--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import 'react-toastify/dist/ReactToastify.css';
-import { HashRouter } from "react-router-dom";
+import dbserver from '../db.json';
+import therapistserver from '../therapist.json';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    
     <React.StrictMode>
+      
+      <dbserver/>
+      <therapistserver/>
      
-        <App />  
+        <App /> 
+        
       
     </React.StrictMode>
 
